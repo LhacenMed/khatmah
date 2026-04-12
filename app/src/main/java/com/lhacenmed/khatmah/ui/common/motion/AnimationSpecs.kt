6ef4e44bafe5c4944a846f9087d608dfbb1ed386
lhacenmed <lhacenmed0@gmail.com>
2026-04-12 01:23:18 +0100

    fixes

--- app/src/main/java/com/lhacenmed/khatmah/ui/common/motion/AnimationSpecs.kt
+++ app/src/main/java/com/lhacenmed/khatmah/ui/common/motion/AnimationSpecs.kt
@@ -13,18 +13,20 @@ fun PathInterpolator.toEasing(): Easing {
 
 private val path = Path().apply {
     moveTo(0f, 0f)
-    cubicTo(0.05F, 0F, 0.133333F, 0.06F, 0.166666F, 0.4F)
-    cubicTo(0.208333F, 0.82F, 0.25F, 1F, 1F, 1F)
+    cubicTo(0.07f, 0f, 0.14f, 0.08f, 0.18f, 0.45f)
+    cubicTo(0.22f, 0.75f, 0.28f, 1f, 1f, 1f)
 }
 
 private val emphasizePathInterpolator = PathInterpolator(path)
 val emphasizeEasing = emphasizePathInterpolator.toEasing()
-private val emphasizeEasingVariant = CubicBezierEasing(.2f, 0f, 0f, 1f)
-private val emphasizedDecelerate = CubicBezierEasing(0.05f, 0.7f, 0.1f, 1f)
-private val emphasizedAccelerate = CubicBezierEasing(0.3f, 0f, 1f, 1f)
 
-private val standardDecelerate = CubicBezierEasing(.0f, .0f, 0f, 1f)
+private val emphasizeEasingVariant = CubicBezierEasing(0.25f, 0f, 0.1f, 1f)
+private val emphasizedDecelerate = CubicBezierEasing(0.1f, 0.75f, 0.2f, 1f)
+private val emphasizedAccelerate = CubicBezierEasing(0.35f, 0f, 0.9f, 1f)
+private val standardDecelerate = CubicBezierEasing(0f, 0f, 0.1f, 1f)
+private val motionEasingStandard = CubicBezierEasing(0.35f, 0f, 0.25f, 1f)
 
-private val motionEasingStandard = CubicBezierEasing(0.4F, 0.0F, 0.2F, 1F)
-
-private val tweenSpec = tween<Float>(durationMillis = DURATION_ENTER, easing = emphasizeEasing)
+private val tweenSpec = tween<Float>(
+    durationMillis = DURATION_ENTER,
+    easing = motionEasingStandard
+)
