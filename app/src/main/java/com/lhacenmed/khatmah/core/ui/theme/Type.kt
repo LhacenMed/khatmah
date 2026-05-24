d0dbeb7a2e2de433d8e73f7d11281ae667234803
lhacenmed <lhacenmed0@gmail.com>
2026-05-24 06:55:09 +0100

    Remove unnecessary fonts. Add Hafs fonts.

--- app/src/main/java/com/lhacenmed/khatmah/core/ui/theme/Type.kt
+++ app/src/main/java/com/lhacenmed/khatmah/core/ui/theme/Type.kt
@@ -8,36 +8,10 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDirection
 import com.lhacenmed.khatmah.R
 
-val OutfitFamily = FontFamily(Font(R.font.outfit_regular))
-val AmiriQuranFamily = FontFamily(Font(R.font.amiri_regular))
-val WarshFamily = FontFamily(Font(R.font.warsh_regular))
-val AlmaghribiWarshFamily = FontFamily(Font(R.font.almaghribi_warsh))
-val UthmaniWarshFamily = FontFamily(Font(R.font.uthmani_warsh))
-val WarshTharwatEmaraFamily = FontFamily(Font(R.font.warsh_tharwat_emara))
+val WarshFamily = FontFamily(Font(R.font.kfgqpc_warsh_uthmanic))
 val WarshSuraNameFamily = FontFamily(Font(R.font.warsh_sura_name))
-val SurahHeaderFamily = FontFamily(Font(R.font.surah_header_color_regular))
-val KFGQPCFamily = FontFamily(
-    Font(R.font.kfgqpc_warsh_v2_regular, FontWeight.Normal),
-    Font(R.font.kfgqpc_warsh_v2_bold, FontWeight.Bold),
-)
-val DinNextLtFamily = FontFamily(
-    Font(R.font.din_next_lt_ultra_light, FontWeight.ExtraLight),
-    Font(R.font.din_next_lt_light,       FontWeight.Light),
-    Font(R.font.din_next_lt_regular,     FontWeight.Normal),
-    Font(R.font.din_next_lt_bold,        FontWeight.Bold),
-    Font(R.font.din_next_lt_heavy,       FontWeight.ExtraBold),
-    Font(R.font.din_next_lt_black,       FontWeight.Black),
-)
-val CairoFamily = FontFamily(
-    Font(R.font.cairo_extra_light, FontWeight.ExtraLight),
-    Font(R.font.cairo_light,       FontWeight.Light),
-    Font(R.font.cairo_regular,     FontWeight.Normal),
-    Font(R.font.cairo_bold,        FontWeight.Bold),
-    Font(R.font.cairo_extra_bold,  FontWeight.ExtraBold),
-    Font(R.font.cairo_black,       FontWeight.Black),
-    Font(R.font.cairo_medium,       FontWeight.Medium),
-    Font(R.font.cairo_semi_bold,       FontWeight.SemiBold),
-)
+val HafsFamily = FontFamily(Font(R.font.kfgqpc_hafs_uthmanic))
+val HafsSuraNameFamily = FontFamily(Font(R.font.hafs_sura_name))
 val NotoKufiFamily = FontFamily(
     Font(R.font.noto_kufi_extra_light, FontWeight.ExtraLight),
     Font(R.font.noto_kufi_light,       FontWeight.Light),
