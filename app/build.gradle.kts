97061f8c3a279e208cdd5f931ea9518443e06e25
lhacenmed <lhacenmed0@gmail.com>
2026-06-24 16:39:45 +0100

    release: v1.0.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,7 @@ sealed class Version(
 val currentVersion: Version = Version.Stable(
     versionMajor = 1,
     versionMinor = 0,
-    versionPatch = 6,
+    versionPatch = 7,
 )
 
 val keystorePropertiesFile: File = rootProject.file("keystore.properties")
